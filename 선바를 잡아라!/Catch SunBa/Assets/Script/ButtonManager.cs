be4b0c60e4f098d4d5deed7d14cbe409c3626bb5
jaehuian <email>
2021-01-12 13:47:28 +0900

    선바를 잡아라 커밋

---
+++ 선바를 잡아라!/Catch SunBa/Assets/Script/ButtonManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//장면 로드
+using UnityEngine.SceneManagement;
+public class ButtonManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartButtonClick()
+    {
+        //게임화면 불러오기
+        SceneManager.LoadScene("Game");
+        Debug.Log("Button Click");
+    }
+}
---
+++ 선바를 잡아라!/Catch SunBa/Assets/Script/SoundManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    AudioSource sound;
+    public AudioClip bee;
+    public AudioClip alcohol;
+    public AudioClip sunba;
+    public AudioClip angry;
+
+    private void Awake()
+    {
+        sound = this.GetComponent<AudioSource>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void play_bee()
+    {
+        sound.PlayOneShot(bee);
+    }
+
+    public void play_alcohol()
+    {
+        sound.PlayOneShot(alcohol);
+    }
+    public void play_sunba()
+    {
+        sound.PlayOneShot(sunba);
+    }
+    public void play_angry()
+    {
+        sound.PlayOneShot(angry);
+    }
+}
